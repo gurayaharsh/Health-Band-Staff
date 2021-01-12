2216a923c2d337a60b27a0ed0a31bbaddff9f508
gurayaharsh <gurayah@mcmaster.ca>
2021-01-12 15:04:19 -0500

    initial project

---
+++ Health Band Staff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Health Band Staff
+//
+//  Created by Harsh  on 2021-01-12.
+//  Copyright © 2021 Harsh Guraya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
