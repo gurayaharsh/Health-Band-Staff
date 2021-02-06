0a63966651c1126847230c855f55cfd9514f7b8b
joshuageorge <georgj7@mcmaster.ca>
2021-02-06 14:07:34 -0500

    created patient screen with patient table

--- Health Band Staff/Controllers/DashboardViewController.swift
+++ Health Band Staff/Controllers/DashboardViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 import Firebase
 
-class DashBoardViewController: UIViewController {
+class DashBoardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var patientTableView: UITableView!
+    let myData = ["first", "second", "third", "fourth", "fifth"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "DashBoard"
         navigationItem.hidesBackButton = true
+        
+        // setting background image
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background")
+        backgroundImage.contentMode = .scaleAspectFill
+        view.insertSubview(backgroundImage, at: 0)
+        
+        
+        // Patient Table
+        let nib = UINib(nibName: "PatientTableViewCell", bundle: nil)
+        patientTableView.register(nib, forCellReuseIdentifier: "PatientTableViewCell")
+        patientTableView.delegate = self
+        patientTableView.dataSource = self
     }
+    
     @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
         do {
           try Auth.auth().signOut()
@@ -24,5 +41,16 @@ class DashBoardViewController: UIViewController {
           print (signOutError)
         }
           
+    }
+    
+    // TableView Funcs
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PatientTableViewCell", for: indexPath) as! PatientTableViewCell
+        cell.myLabel.text = myData[indexPath.row]
+        return cell
     }
 }
